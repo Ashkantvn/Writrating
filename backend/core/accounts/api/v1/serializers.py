d2491bcbf73a99f153a21f8743e5035a98f92bdc
Ashkan <ashkantavangar7@gmail.com>
2025-01-07 12:19:25 +0330

    Create profile api (serializer and view)

--- backend/core/accounts/api/v1/serializers.py
+++ backend/core/accounts/api/v1/serializers.py
@@ -1,6 +1,20 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
+from accounts.models import Profile
 
 
+class ProfileSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Profile
 
-class ProfileSerializer(ModelSerializer):
-    pass
+        fields = ["id","username","profile_image","first_name","last_name","description"]
+        read_only_fields = ["id"]
+
+    def validate_username(self,value):
+        invalid_chars = set('!@#$%^&*()+=[]{}|\\:;"\'<>,.?/')
+        if " " in value:
+            raise serializers.ValidationError("Username should not contain spaces")
+        elif any(char in invalid_chars for char in value):
+            raise serializers.ValidationError("Username contains invalid characters")
+        else:
+            return value
--- backend/core/accounts/api/v1/views.py
+++ backend/core/accounts/api/v1/views.py
@@ -1,27 +1,50 @@
 from django.shortcuts import render
-from rest_framework.generics import GenericAPIView
+from accounts.api.v1.serializers import ProfileSerializer
+from accounts.models.profile_model import Profile
+from rest_framework.response import Response
+from rest_framework import status , response, generics , mixins
+from django.shortcuts import get_object_or_404
+from accounts.api.v1.permissions import CustomIsAuthenticatedOrReadOnly
 
 # Create your views here.
-class ProfileAPI(GenericAPIView):
+class ProfileAPI(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin):
+    """
+        Profile api let users see users profile and edit their profile .
+        Methods: GET , PATCH
+    """
+    permission_classes = [CustomIsAuthenticatedOrReadOnly]
+    serializer_class = ProfileSerializer
+    queryset = Profile.objects.all()
+    lookup_field = "username"
+
+        
+    def get(self,request,username,format=None):
+        return self.retrieve(request=request)
+    
+    def patch(self,request,username,format=None,*args,**kwargs):
+        profile = get_object_or_404(Profile,username=username)
+        if  profile.user.email != request.user.email:
+            return Response(data={"detail":"You can only change your profile"},status=status.HTTP_403_FORBIDDEN)
+        return self.partial_update(request=request,*args,**kwargs)
+
+
+class SignUpAPI(generics.GenericAPIView):
     pass
 
-class SignUpAPI(GenericAPIView):
+class LoginAPI(generics.GenericAPIView):
     pass
 
-class LoginAPI(GenericAPIView):
+class LogoutAPI(generics.GenericAPIView):
     pass
 
-class LogoutAPI(GenericAPIView):
+class DeleteAccountAPI(generics.GenericAPIView):
     pass
 
-class DeleteAccountAPI(GenericAPIView):
+class ChangePassAPI(generics.GenericAPIView):
     pass
 
-class ChangePassAPI(GenericAPIView):
+class PasswordRecoveryAPI(generics.GenericAPIView):
     pass
 
-class PasswordRecoveryAPI(GenericAPIView):
-    pass
-
-class PasswordRecoveryVerificationAPI(GenericAPIView):
+class PasswordRecoveryVerificationAPI(generics.GenericAPIView):
     pass
