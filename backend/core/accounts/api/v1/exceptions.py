9c2fbca182864b97bc0166f1b49e93c0554d93c5
Ashkan <ashkantavangar7@gmail.com>
2025-01-07 12:10:42 +0330

    Create permission and exception for profile

---
+++ backend/core/accounts/api/v1/exceptions.py
@@ -0,0 +1,8 @@
+from rest_framework.exceptions import APIException
+from rest_framework import status 
+
+
+class CustomAuthenticationFailed(APIException):
+    status_code = status.HTTP_401_UNAUTHORIZED
+    default_detail = "Authentication credentials were not provided."
+    default_code = "authentication_failed"
---
+++ backend/core/accounts/api/v1/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+from accounts.api.v1.exceptions import CustomAuthenticationFailed
+from rest_framework import status
+
+
+class CustomIsAuthenticatedOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
+            return True
+        elif not request.user or not request.user.is_authenticated:
+            raise  CustomAuthenticationFailed()
+        else:
+            return True
