18dab640314190f09ddcfa37521ddec2afc8155c
Ashkan <ashkantavangar7@gmail.com>
2025-01-26 12:16:40 +0330

    Update folder structure of blogs and Create url of blogs list

--- backend/core/blogs/api/v1/urls.py
+++ backend/core/blogs/api/v1/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path
+from blogs.api.v1 import views
 
 
+app_name = 'blogs'
 
 urlpatterns = [
-    # path('lists/', BlogListAPIView.as_view(), name='list'),
-    # path('add/', BlogAddAPIView.as_view(), name='add'),
-    # path('<str:slug>/', BlogRetrieveAPIView.as_view(), name='details'),
-    # path('<str:slug>/edit/', BlogEditAPIView.as_view(), name='edit'),
-    # path('<str:slug>/delete/', BlogDeleteAPIView.as_view(), name='delete'),
-    # path('<str:slug/check/', BlogCheckAPIView.as_view(), name='check'),
+    path('lists/', views.BlogListAPIView.as_view(), name='list'),
+    # path('add/', views.BlogAddAPIView.as_view(), name='add'),
+    # path('<str:slug>/', views.BlogRetrieveAPIView.as_view(), name='details'),
+    # path('<str:slug>/edit/', views.BlogEditAPIView.as_view(), name='edit'),
+    # path('<str:slug>/delete/', views.BlogDeleteAPIView.as_view(), name='delete'),
+    # path('<str:slug/check/', views.BlogCheckAPIView.as_view(), name='check'),
 ]
---
+++ backend/core/blogs/tests/test_urls.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from blogs.api.v1 import views
+
+class TestUrls(SimpleTestCase):
+
+    def test_blogs_list_url_is_resolved(self):
+        url = reverse('blogs:list')
+        view_class = resolve(url).func.view_class
+        self.assertEqual(view_class, views.BlogListAPIView)
