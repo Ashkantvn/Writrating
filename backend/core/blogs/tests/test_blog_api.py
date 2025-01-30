b4c388279eba2fb36d03f1b5d087405b2eed4d37
Ashkan <ashkantavangar7@gmail.com>
2025-01-30 12:44:33 +0330

    Create test for blog list

---
+++ backend/core/blogs/tests/test_blog_api.py
@@ -0,0 +1,14 @@
+import pytest
+from rest_framework.test import APIClient
+from django.urls import reverse
+
+
+
+@pytest.mark.django_db
+class TestBlogListAPI:
+
+    def test_GET_blog_list_status_200(self):
+        client = APIClient()
+        url = reverse('blogs:list')
+        response = client.get(url)
+        assert response.status_code == 200
