3d1a336041c25be0acef0fb7c940f723cbf86c99
Ashkan <ashkantavangar7@gmail.com>
2025-01-30 12:29:37 +0330

    Update blog admin panel

--- backend/core/blogs/admin.py
+++ backend/core/blogs/admin.py
@@ -1,15 +1,26 @@
 from django.contrib import admin
 from blogs.models import Blog
 
+# Actions for the admin panel
+def make_publishable(modeladmin, request, queryset):
+    queryset.update(publishable=True)
+
+make_publishable.short_description = "Mark selected blogs as publishable"
+
+def make_unpublishable(modeladmin, request, queryset):
+    queryset.update(publishable=False)
+
+make_unpublishable.short_description = "Mark selected blogs as unpublishable"
+
 # Register your models here.
 @admin.register(Blog)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'create_date']
     search_fields = ['title', 'author']
-    list_filter = ['create_date']
+    list_filter = ['create_date','published_date','publishable']
     date_hierarchy = 'published_date'
     ordering = ['create_date']
-    readonly_fields = ['create_date', 'update_date','views']
+    readonly_fields = ['create_date', 'update_date','views','slug']
     fieldsets = (
         ('Blog Information', {
             'fields': (
@@ -25,3 +36,4 @@ class BlogAdmin(admin.ModelAdmin):
                 )
         }),
     )
+    actions = [make_publishable, make_unpublishable]
