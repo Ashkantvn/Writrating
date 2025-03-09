1ce46d06fe21bd05999bf26fae4a245b0c37754b
Ashkan <ashkantavangar7@gmail.com>
2025-03-09 11:20:10 +0330

    Complete tests for retrieve blog api

---
+++ backend/core/blogs/tests/__init__.py
@@ -0,0 +1,4 @@
+from blogs.tests.test_list_blog_api import TestBlogListAPI
+from blogs.tests.test_retrieve_blog_api import TestRetrieveBlogApi
+from blogs.tests.test_models import TestTagModel,TestBlogModel,TestCategoryModel
+from blogs.tests.test_urls  import TestUrls
--- backend/core/blogs/tests/test_retrieve_blog_api.py
+++ backend/core/blogs/tests/test_retrieve_blog_api.py
@@ -1,21 +1,30 @@
 import pytest
 from django.urls import reverse
-
-
-
+from blogs.tests.fixtures import blog
+from rest_framework.test import APIClient
+from rest_framework import status
 
 
 @pytest.mark.django_db
-class RetrieveBlogApi:
+class TestRetrieveBlogApi:
 
-    def test_GET_retrieve_blog_status_200(self):
-        pass
+    def test_GET_retrieve_blog_status_200(self,blog):
+        client = APIClient()
+        url = reverse('blogs:details',kwargs={'slug':blog.slug})
+        response = client.get(url)
+        assert response.status_code == status.HTTP_200_OK
+        assert response.data['title'] == blog.title
+        assert response.data['content'] == blog.content
+        
 
     def test_GET_retrieve_blog_status_404(self):
-        pass
+        client = APIClient()
+        url = reverse('blogs:details',kwargs={'slug':'something-else'})
+        response = client.get(url)
+        assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_GET_retrieve_blog_status_401(self):
-        pass
-
-    def test_GET_retrieve_blog_content(self):
-        pass
+    def test_GET_retrieve_blog_status_400(self,blog):
+        client = APIClient()
+        url = reverse('blogs:details',kwargs={'slug':blog.slug})
+        response = client.get(url)
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
