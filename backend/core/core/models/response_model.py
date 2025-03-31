e15d48267c000e49690575de1e7926ce0e214db2
Ashkan <ashkantavangar7@gmail.com>
2025-03-31 18:16:34 +0330

    Create abstract response model

---
+++ backend/core/core/models/response_model.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class Response(models.Model):
+    """
+    Model to store the response of the checkings.
+    """
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    
+    class Meta:
+        abstract = True
+        ordering = ['title']
