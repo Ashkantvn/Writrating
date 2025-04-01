60996c0c154a20fb81c420862dfedd2ed59d82ed
Ashkan <ashkantavangar7@gmail.com>
2025-04-01 20:36:27 +0330

    Add initial urls

--- backend/core/devices/api/v1/urls.py
+++ backend/core/devices/api/v1/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
+from devices.api.v1 import views
 
-
-
+app_name = "devices"
 
 urlpatterns = [
-    
+    path('',views.DevicesListAPIView.as_view(), name='list'),
+    path('add/',views.DevicesCreateAPIView.as_view(), name='create'),
+    path('<str:slug>/',views.DevicesDetailAPIView.as_view(), name='details'),
+    path('<str:slug>/edit/',views.DevicesUpdateAPIView.as_view(), name='update'),
 ]
