3e82ec665aaf32ba4d3ed1485322d20013c18684
Ashkan <ashkantavangar7@gmail.com>
2025-04-01 21:05:05 +0330

    Generate initial models for device api

---
+++ backend/core/devices/models/device_other_info_model.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+
+
+class DeviceImages(models.Model):
+    pass
+
+class DeviceFeatures(models.Model):
+    pass
+
+class DeviceSoftwareInformation(models.Model):
+    pass
+
+class DeviceHardwareInformation(models.Model):
+    pass
+
+class DevicePhysicalInformation(models.Model):
+    pass
+
+class DeviceCategory(models.Model):
+    pass
---
+++ backend/core/devices/models/hardware_other_info_model.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+
+class Processor(models.Model):
+    pass
+
+
+class GraphicProcessor(models.Model):
+    pass
+
+class Camera(models.Model):
+    pass
+
+class Display(models.Model):
+    pass
