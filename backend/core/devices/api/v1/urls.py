a3bd5d43d1f476aed8f2bc836c274712038cb9c2
Ashkan <ashkantavangar7@gmail.com>
2025-04-01 20:41:01 +0330

    update urls of devices api

--- backend/core/devices/api/v1/urls.py
+++ backend/core/devices/api/v1/urls.py
@@ -4,8 +4,10 @@ from devices.api.v1 import views
 app_name = "devices"
 
 urlpatterns = [
-    path('',views.DevicesListAPIView.as_view(), name='list'),
-    path('add/',views.DevicesCreateAPIView.as_view(), name='create'),
-    path('<str:slug>/',views.DevicesDetailAPIView.as_view(), name='details'),
-    path('<str:slug>/edit/',views.DevicesUpdateAPIView.as_view(), name='update'),
+    # path('',views.DevicesListAPIView.as_view(), name='list'),
+    # path('add/',views.DevicesCreateAPIView.as_view(), name='create'),
+    # path('<str:slug>/',views.DevicesDetailAPIView.as_view(), name='details'),
+    # path('<str:slug>/edit/',views.DevicesUpdateAPIView.as_view(), name='update'),
+    # path('<str:slug>/delete/',views.DevicesDeleteAPIView.as_view(), name='delete'),
+    # path('<str:slug>/check/',views.DevicesCheckAPIView.as_view(), name='check'),
 ]
