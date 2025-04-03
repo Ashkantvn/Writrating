d9b9db44d38375f82c3b42a3cc4ec6fce5342227
Ashkan <ashkantavangar7@gmail.com>
2025-04-03 14:29:33 +0330

    Update devices models

---
+++ backend/core/devices/models/__init__.py
@@ -0,0 +1,16 @@
+from .device_model import Device
+from .device_other_info_model import (
+    DeviceCategory,
+    DeviceFeatures,
+    DeviceHardwareInformation,
+    DeviceImages,
+    DevicePhysicalInformation,
+    DeviceSoftwareInformation
+    )
+from .hardware_other_info_model import (
+    Display, 
+    Camera, 
+    Processor, 
+    GraphicProcessor
+    )
+from .software_other_info_model import OperatingSystem
