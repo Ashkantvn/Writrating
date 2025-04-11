86a3fe628c27da87a5143f3c5e120ab6eeaa6882
Ashkan <ashkantavangar7@gmail.com>
2025-04-11 13:54:21 +0330

    Generate list api for devices

--- backend/core/devices/api/v1/urls.py
+++ backend/core/devices/api/v1/urls.py
@@ -4,7 +4,7 @@ from devices.api.v1 import views
 app_name = "devices"
 
 urlpatterns = [
-    # path('',views.DevicesListAPIView.as_view(), name='list'),
+    path('',views.DevicesListAPIView.as_view(), name='list'),
     # path('add/',views.DevicesCreateAPIView.as_view(), name='create'),
     # path('<str:slug>/',views.DevicesDetailAPIView.as_view(), name='details'),
     # path('<str:slug>/edit/',views.DevicesUpdateAPIView.as_view(), name='update'),
---
+++ backend/core/devices/api/v1/views.py
@@ -0,0 +1,15 @@
+from rest_framework.views import APIView
+from devices.models import Device
+from devices.api.v1 import serializers
+from rest_framework import status
+from rest_framework.response import Response
+
+class DevicesListAPIView(APIView):
+    
+    def get(self, request):
+        devices = Device.objects.all()
+        serializer = serializers.DeviceSerializer(devices,many=True)
+        if devices.count() == 0:
+            return Response({"data":[],"detail":"No devices found"}, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.data, status=status.HTTP_200_OK)
