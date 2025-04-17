ce66ac9d675159b7cc78f5a73353d1e58b154b3f
Ashkan <ashkantavangar7@gmail.com>
2025-04-17 21:46:47 +0330

    reformat code

--- backend/core/core/permissions.py
+++ backend/core/core/permissions.py
@@ -37,15 +37,15 @@ class IsValidator(BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return getattr(request.user, "is_validator", False)
-    
+
 
 class IsValidatorForGET(BasePermission):
     def has_permission(self, request, view):
-        if request.method == "GET" and getattr(request.user, "is_validator", False)==False:
+        if request.method == "GET" and not getattr(request.user, "is_validator", False):
             return False
         return True
 
     def has_object_permission(self, request, view, obj):
-        if request.method == "GET" and getattr(request.user, "is_validator", False)==False:
+        if request.method == "GET" and not getattr(request.user, "is_validator", False):
             return False
         return True
